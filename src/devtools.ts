968d25fcb2ee4c1226c920e44ac761c022133388
Luca Forstner <luca.forstner@sentry.io>
2025-05-06 11:37:46 +0200

    LRU

--- src/devtools.ts
+++ src/devtools.ts
@@ -1,5 +1,35 @@
 import * as JSZip from "jszip";
 
+class LRUCache<K, V> {
+  private maxSize: number;
+  private cache = new Map<K, V>();
+  constructor(maxSize: number) {
+    this.maxSize = maxSize;
+  }
+  get(key: K): V | undefined {
+    const val = this.cache.get(key);
+    if (!val) return undefined;
+    this.cache.delete(key);
+    this.cache.set(key, val);
+    return val;
+  }
+  set(key: K, value: V): void {
+    if (this.cache.has(key)) this.cache.delete(key);
+    this.cache.set(key, value);
+    if (this.cache.size > this.maxSize) {
+      const oldestKey = this.cache.keys().next().value;
+      // @ts-expect-error we don't care
+      this.cache.delete(oldestKey);
+    }
+  }
+  clear(): void {
+    this.cache.clear();
+  }
+}
+
+const zipCache = new LRUCache<string, JSZip>(200);
+const artifactCache = new LRUCache<string, { url: string }[]>(200);
+
 interface Config {
   sentryAuthToken: string | null;
   projectConfigs: ProjectConfig[];
@@ -60,146 +90,158 @@ function findMatchingConfigs(url: string): ProjectConfig[] {
 // Initialize configuration when script loads
 loadConfig();
 
-// Listen for resources being added to the page
-chrome.devtools.inspectedWindow.onResourceAdded.addListener(
-  async (resource) => {
-    const tab = await chrome.tabs.get(chrome.devtools.inspectedWindow.tabId);
+async function processResource(
+  resource: chrome.devtools.inspectedWindow.Resource
+) {
+  const tab = await chrome.tabs.get(chrome.devtools.inspectedWindow.tabId);
 
-    if (!tab || !tab.url) {
-      return;
-    }
+  if (!tab || !tab.url) {
+    return;
+  }
 
-    const currentConfig = await loadConfig();
-    if (!currentConfig.sentryAuthToken) {
-      return;
-    }
+  const currentConfig = await loadConfig();
+  if (!currentConfig.sentryAuthToken) {
+    return;
+  }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if ((resource as any).type !== "script") {
+    return;
+  }
+
+  const matchingConfigs = findMatchingConfigs(tab.url);
+
+  if (matchingConfigs.length === 0) {
+    return;
+  }
+
+  const resourceBuildId =
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if ((resource as any).type !== "script") {
-      return;
-    }
-
-    const matchingConfigs = findMatchingConfigs(tab.url);
-
-    if (matchingConfigs.length === 0) {
-      return;
-    }
-
-    const resourceBuildId =
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      ((resource as any).buildId as string | undefined) ??
-      (await new Promise((resolve) => {
-        resource.getContent((content, encoding) => {
-          if (!content) {
-            return resolve(undefined);
-          }
-
-          const decodedContent = encoding ? atob(content) : content;
-
-          const sentryMagicExpression = decodedContent.match(
-            /sentry-dbid-([0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})/
-          );
-
-          if (sentryMagicExpression) {
-            return resolve(sentryMagicExpression[1]);
-          }
-
-          const debugIdMagicComment = decodedContent.match(
-            /^\s*\/\/#\s?debugId=(.*)$/m
-          );
-
-          if (debugIdMagicComment) {
-            return resolve(debugIdMagicComment[1]);
-          }
-
+    (resource as any).buildId ??
+    (await new Promise((resolve) => {
+      resource.getContent((content, encoding) => {
+        if (!content) {
           return resolve(undefined);
-        });
-      }));
+        }
 
-    if (!resourceBuildId) {
+        const decodedContent = encoding ? atob(content) : content;
+
+        const sentryMagicExpression = decodedContent.match(
+          /sentry-dbid-([0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})/
+        );
+
+        if (sentryMagicExpression) {
+          return resolve(sentryMagicExpression[1]);
+        }
+
+        const debugIdMagicComment = decodedContent.match(
+          /^\s*\/\/#\s?debugId=(.*)$/m
+        );
+
+        if (debugIdMagicComment) {
+          return resolve(debugIdMagicComment[1]);
+        }
+
+        return resolve(undefined);
+      });
+    }));
+
+  if (!resourceBuildId) {
+    return;
+  }
+
+  try {
+    const { organization: orgSlug, project: projectSlug } = matchingConfigs[0];
+
+    const headers = {
+      Authorization: `Bearer ${currentConfig.sentryAuthToken}`,
+    };
+
+    const lookupUrl = `https://sentry.io/api/0/projects/${orgSlug}/${projectSlug}/artifact-lookup/?debug_id=${resourceBuildId}`;
+
+    const cacheKeyLookup = lookupUrl.toString();
+    let artifactBundlesWithDebugID = artifactCache.get(cacheKeyLookup);
+    if (!artifactBundlesWithDebugID) {
+      const artifactLookupResponse = await fetch(lookupUrl, { headers });
+      artifactBundlesWithDebugID = (await artifactLookupResponse.json()) as {
+        url: string;
+      }[];
+      artifactCache.set(cacheKeyLookup, artifactBundlesWithDebugID);
+    }
+
+    const bundleInfo = artifactBundlesWithDebugID[0];
+    if (!bundleInfo) {
       return;
     }
 
-    try {
-      const { organization: orgSlug, project: projectSlug } =
-        matchingConfigs[0];
+    const zipUrl = new URL(bundleInfo.url);
+    zipUrl.hostname = "sentry.io";
+    zipUrl.protocol = "https:";
+    zipUrl.port = "";
 
-      const headers = {
-        Authorization: `Bearer ${currentConfig.sentryAuthToken}`,
-      };
-
-      const lookupUrl = `https://sentry.io/api/0/projects/${orgSlug}/${projectSlug}/artifact-lookup/?debug_id=${resourceBuildId}`;
-
-      const artifactLookupResponse = await fetch(lookupUrl, { headers });
-
-      const artifactBundlesWithDebugID =
-        (await artifactLookupResponse.json()) as {
-          url: string;
-        }[];
-
-      const bundleInfo = artifactBundlesWithDebugID[0];
-      if (!bundleInfo) {
-        return;
-      }
-
-      const zipUrl = new URL(bundleInfo.url);
-      zipUrl.hostname = "sentry.io";
-      zipUrl.protocol = "https:";
-      zipUrl.port = "";
-
+    const cacheKey = zipUrl.toString();
+    let zip = zipCache.get(cacheKey);
+    if (!zip) {
       const zipFileResponse = await fetch(zipUrl.toString(), { headers });
       const zipBlob = await zipFileResponse.blob();
-
-      const zip = await JSZip.loadAsync(zipBlob);
-
-      const manifestStringContent = await zip
-        .file("manifest.json")
-        ?.async("string");
-      if (!manifestStringContent) {
-        return;
-      }
-
-      let manifest: {
-        files: Record<
-          string,
-          { type: string; headers: { "debug-id": string } }
-        >;
-      };
-      try {
-        manifest = JSON.parse(manifestStringContent);
-      } catch (err) {
-        console.error("[JSZip] Failed to parse manifest.json:", err);
-        return;
-      }
-
-      const zipPathForSourcemap = Object.entries(manifest.files).find(
-        ([, info]) =>
-          info.type === "source_map" &&
-          info.headers["debug-id"] === resourceBuildId
-      )?.[0];
-
-      if (!zipPathForSourcemap) {
-        return;
-      }
-
-      const sourceMapStringContent = await zip
-        .file(zipPathForSourcemap)
-        ?.async("string");
-      if (!sourceMapStringContent) {
-        return;
-      }
-
-      // This looks abysmal but not doing the encode unescape dance results in errors where certain characters are out of base64 range
-      const base64SourceMap = btoa(
-        unescape(encodeURIComponent(sourceMapStringContent))
-      );
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (resource as any).attachSourceMapURL(
-        `data:application/json;base64,${base64SourceMap}`
-      );
-    } catch (err) {
-      console.error("[onResourceAdded] Error in source map flow:", err);
+      zip = await JSZip.loadAsync(zipBlob);
+      zipCache.set(cacheKey, zip);
     }
+
+    const manifestStringContent = await zip
+      .file("manifest.json")
+      ?.async("string");
+    if (!manifestStringContent) {
+      return;
+    }
+
+    let manifest: {
+      files: Record<string, { type: string; headers: { "debug-id": string } }>;
+    };
+    try {
+      manifest = JSON.parse(manifestStringContent);
+    } catch (err) {
+      console.error("[JSZip] Failed to parse manifest.json:", err);
+      return;
+    }
+
+    const zipPathForSourcemap = Object.entries(manifest.files).find(
+      ([, info]) =>
+        info.type === "source_map" &&
+        info.headers["debug-id"] === resourceBuildId
+    )?.[0];
+
+    if (!zipPathForSourcemap) {
+      return;
+    }
+
+    const sourceMapStringContent = await zip
+      .file(zipPathForSourcemap)
+      ?.async("string");
+    if (!sourceMapStringContent) {
+      return;
+    }
+
+    const base64SourceMap = btoa(
+      unescape(encodeURIComponent(sourceMapStringContent))
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (resource as any).attachSourceMapURL(
+      `data:application/json;base64,${base64SourceMap}`
+    );
+  } catch (err) {
+    console.error("[processResource] Error in source map flow:", err);
   }
-);
+}
+
+chrome.devtools.inspectedWindow.getResources((resources) => {
+  resources.forEach(processResource);
+});
+
+chrome.devtools.inspectedWindow.onResourceAdded.addListener(processResource);
+
+window.addEventListener("unload", () => {
+  zipCache.clear();
+  artifactCache.clear();
+});
